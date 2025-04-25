0a0858fcf44738077c0e72008a97dcdcc0d84a3f
Nick Chen <nick.chen@cmp.com.tw>
2025-04-25 14:50:33 +0800

    feat: add web client

---
+++ src/main/java/com/niqdev/app/config/WebClientConfig.java
@@ -0,0 +1,14 @@
+package com.niqdev.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+	@Bean
+	WebClient webClient(WebClient.Builder builder) {
+	    return builder.build();
+	}
+}
--- src/main/java/com/niqdev/app/controller/HomeController.java
+++ src/main/java/com/niqdev/app/controller/HomeController.java
@@ -11,9 +11,13 @@ import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private WebClient webClient;
 	
 	@Autowired
     private OAuth2AuthorizedClientService authorizedClientService;
@@ -44,5 +48,80 @@ public class HomeController {
     	}
     	
         return "home";
+    }
+    
+    @GetMapping("/getApiJwt")
+    public String getApiJwt(Model model, OAuth2AuthenticationToken authentication) {
+    	
+    	OAuth2AuthorizedClient client = null;
+    	
+    	if (authentication != null) {
+    		client = authorizedClientService.loadAuthorizedClient("my-client", authentication.getName());
+    	}
+    	
+    	if (client != null) {
+            String accessToken = client.getAccessToken().getTokenValue();
+            
+            String result = webClient.get()
+                    .uri("http://localhost:8082/api/jwt")
+                    .header("Authorization", "Bearer " + accessToken)
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .block();
+
+            model.addAttribute("result", result);
+      	}
+
+        return "api-result";
+    }
+    
+    @GetMapping("/getApiUser")
+    public String getApiUser(Model model, OAuth2AuthenticationToken authentication) {
+    	
+    	OAuth2AuthorizedClient client = null;
+    	
+    	if (authentication != null) {
+    		client = authorizedClientService.loadAuthorizedClient("my-client", authentication.getName());
+    	}
+    	
+    	if (client != null) {
+            String accessToken = client.getAccessToken().getTokenValue();
+            
+            String result = webClient.get()
+                    .uri("http://localhost:8082/api/user")
+                    .header("Authorization", "Bearer " + accessToken)
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .block();
+
+            model.addAttribute("result", result);
+      	}
+
+        return "api-result";
+    }
+    
+    @GetMapping("/getApiRoles")
+    public String getApiRoles(Model model, OAuth2AuthenticationToken authentication) {
+    	
+    	OAuth2AuthorizedClient client = null;
+    	
+    	if (authentication != null) {
+    		client = authorizedClientService.loadAuthorizedClient("my-client", authentication.getName());
+    	}
+    	
+    	if (client != null) {
+            String accessToken = client.getAccessToken().getTokenValue();
+            
+            String result = webClient.get()
+                    .uri("http://localhost:8082/api/roles")
+                    .header("Authorization", "Bearer " + accessToken)
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .block();
+
+            model.addAttribute("result", result);
+      	}
+
+        return "api-result";
     }
 }
