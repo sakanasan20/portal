06336a835ced085f368f37ee7c11965bc13e8578
Nick Chen <nick.chen@cmp.com.tw>
2025-04-25 11:19:43 +0800

    feat: add roles, authorities, tokens display

--- src/main/java/com/niqdev/app/controller/HomeController.java
+++ src/main/java/com/niqdev/app/controller/HomeController.java
@@ -1,15 +1,48 @@
 package com.niqdev.app.controller;
 
+import java.time.Instant;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+    private OAuth2AuthorizedClientService authorizedClientService;
+	
     @GetMapping("/")
     public String home(Model model, OAuth2AuthenticationToken authentication) {
-    	if (authentication != null) model.addAttribute("userName", authentication.getPrincipal().getName());
-        return "home"; // resources/templates/home.html
+    	
+    	OAuth2AuthorizedClient client = null;
+    	
+    	if (authentication != null) {
+    		client = authorizedClientService.loadAuthorizedClient("my-client", authentication.getName());
+    		
+            OidcUser oidcUser = (OidcUser) authentication.getPrincipal();
+            String username = oidcUser.getFullName(); // or getPreferredUsername()
+            List<String> roles = oidcUser.getClaimAsStringList("roles"); // 自訂 roles claim
+
+            model.addAttribute("username", username);
+            model.addAttribute("roles", roles);
+
+    	}
+    	
+    	if (client != null) {
+          String accessToken = client.getAccessToken().getTokenValue();
+          Instant expiresAt = client.getAccessToken().getExpiresAt();
+          
+          model.addAttribute("accessToken", accessToken);
+          model.addAttribute("expiresAt", expiresAt);
+    	}
+    	
+        return "home";
     }
 }
